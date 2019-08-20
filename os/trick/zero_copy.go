9045f76a88b470aff0c7c23a8a579d20c44d5060
Leo ll <dddddonly8@gmail.com>
2019-08-20 17:56:26 +0800

    os: update trick && add zero copy about byte 2 string

---
+++ os/trick/zero_copy.go
@@ -0,0 +1,66 @@
+package trick
+
+import (
+	"reflect"
+	"unsafe"
+)
+
+// By unsafe pointer
+
+type Data struct {
+	Int32  int32
+	String string
+}
+
+var sizeofUnsafeData = int(unsafe.Sizeof(Data{}))
+
+func Data2Bytes(s *Data) []byte {
+	var x reflect.SliceHeader
+	x.Len = sizeofUnsafeData
+	x.Cap = sizeofUnsafeData
+	x.Data = uintptr(unsafe.Pointer(s))
+	return *(*[]byte)(unsafe.Pointer(&x))
+}
+
+func Bytes2Data(b []byte) *Data {
+	return (*Data)(unsafe.Pointer(
+		(*reflect.SliceHeader)(unsafe.Pointer(&b)).Data,
+	))
+}
+
+
+
+// convert b to string without copy
+func Bytes2StringFast(b []byte) string {
+	return *(*string)(unsafe.Pointer(&b))
+}
+
+func String2ByteFast(s string) []byte {
+	sh := (*reflect.StringHeader)(unsafe.Pointer(&s))
+	bh := reflect.SliceHeader{Data: sh.Data, Len: sh.Len, Cap: 0}
+	return *(*[]byte)(unsafe.Pointer(&bh))
+}
+
+func String2Byte(s string) []byte {
+	return []byte(s)
+}
+
+func Byte2String(b []byte) string {
+	return string(b)
+}
+
+
+
+// returns &s[0], which is not allowed in go
+func stringPointer(s string) unsafe.Pointer {
+	p := (*reflect.StringHeader)(unsafe.Pointer(&s))
+	return unsafe.Pointer(p.Data)
+}
+
+// returns &b[0], which is not allowed in go
+func bytePointer(b []byte) unsafe.Pointer {
+	p := (*reflect.SliceHeader)(unsafe.Pointer(&b))
+	return unsafe.Pointer(p.Data)
+}
+
+
---
+++ os/trick/zero_copy_test.go
@@ -0,0 +1,46 @@
+package trick
+
+import (
+	"bytes"
+	"reflect"
+	"testing"
+)
+
+func TestBytes2Data(t *testing.T) {
+	buf := new(bytes.Buffer)
+	buf.Write(Data2Bytes(&Data{Int32: 32, String: "string"}))
+	d := Bytes2Data(buf.Bytes())
+	t.Log(d)
+}
+
+func TestData2Bytes(t *testing.T) {
+	b := Data2Bytes(&Data{Int32: 32, String: "string"})
+	t.Log(b)
+}
+
+func TestBytes2StringFast(t *testing.T) {
+	s := Bytes2StringFast([]byte("string"))
+	t.Log(s)
+
+	b := String2ByteFast("string")
+	buf := new(bytes.Buffer)
+	buf.Write(b)
+	t.Log(reflect.DeepEqual(buf.Bytes(), b))
+}
+
+func TestString2ByteFast(t *testing.T) {
+	b := String2ByteFast("string")
+	t.Log(b)
+}
+
+func BenchmarkString2ByteFast(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		_ = Bytes2StringFast([]byte("string"))
+	}
+}
+
+func BenchmarkString2Byte(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		_ = Byte2String([]byte("string"))
+	}
+}
